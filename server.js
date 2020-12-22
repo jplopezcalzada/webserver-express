22a5a88feec64b60ea51a3406f492753e989b5dc
Juan Pedro López <jplopezcalzada@gmail.com>
2020-12-22 23:58:44 +0100

    Primer commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+var hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+
+// HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Juan Pedro'
+    });
+
+});
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuachando peticiones en el puerto ${port}`);
+});
